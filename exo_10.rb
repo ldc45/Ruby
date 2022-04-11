bfe74c11dfe0ade118fef9c8bfc5e765c0fc8af5
AGESILAS Ludovic <ldc45@outlook.fr>
2022-04-11 19:48:15 +0200

    exercice 11

--- exo_10.rb
+++
@@ -1,4 +0,0 @@
-puts "Quel est ton  année de naissance?"
-print "> "
-year = gets.chomp.to_i
-puts "En 2017, vous aviez #{2017-year} ans"
---
+++ exo_11.rb
@@ -0,0 +1,6 @@
+puts "Quel votre le nombre ?"
+print "> "
+nb = gets.chomp.to_i
+nb.times do 
+  puts "Salut ça farte"
+end
