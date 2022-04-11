53d851bcc0f3801066d3502af8793a3f72bf134b
AGESILAS Ludovic <ldc45@outlook.fr>
2022-04-11 19:05:38 +0200

    exercice 8

--- exo_08.rb
+++ exo_08.rb
@@ -1,4 +1,7 @@
 puts "Quel est ton prénom ?"
 print "> "
-user_name = gets.chomp
-puts "Bonjour #{user_name}"
+firstname = gets.chomp
+puts " Quel est ton nom ?"
+print "> "
+name = gets.chomp
+puts "Bonjour #{firstname} #{name}"
