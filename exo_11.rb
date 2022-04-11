4b6334a986cfb92422e9e7d1ae06694e2d603dad
AGESILAS Ludovic <ldc45@outlook.fr>
2022-04-11 20:05:08 +0200

    exercice 12

--- exo_11.rb
+++
@@ -1,6 +0,0 @@
-puts "Quel votre le nombre ?"
-print "> "
-nb = gets.chomp.to_i
-nb.times do 
-  puts "Salut ça farte"
-end
---
+++ exo_12.rb
@@ -0,0 +1,6 @@
+puts "Quel votre nombre ?"
+print "> "
+nb = gets.chomp.to_i
+for nb in 0..nb
+  puts nb
+end
